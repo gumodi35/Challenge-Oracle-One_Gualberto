b9014e71de390e9a9f79bc83aab581aca5025e40
gumodi35 <gumodi10@gmail.com>
2022-05-10 14:06:49 -0500

    agregando la funcio de cifrar

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // creamos la funcion para encriptar
 function cifrar() {
-    var texto
+    // declaramos las variables
+    var texto = document.getElementById("inputtexto").ariaValueMax.toLocaleLowerCase();
+    //i es para cambiar masyusculas y minisculas
+    // g cambia toda la linea
+    // m cambia multiples lineas
+    var textoCifrado = texto.replace(/e/igm,"enter");
+    var textoCifrado = textoCifrado.replace(/o/igm,"ober");
+    var textoCifrado = textoCifrado.replace(/i/igm, "imes");
+    var textoCifrado = textoCifrado.replace(/a/igm,"ai");
+    var textoCifrado = textoCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("texto2").innerHTML = textoCifrado;
+    document.getElementById("inputtexto").innerHTML = " ";
+    document.getElementById("copiar").style.display = "inherit";
 }
