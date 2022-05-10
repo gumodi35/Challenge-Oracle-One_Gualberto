927d8108bcd265587db990188baa55f1368a0bf4
gumodi35 <gumodi10@gmail.com>
2022-05-10 14:42:36 -0500

    agregando la funcion de descifrar

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function cifrar() {
     //i es para cambiar masyusculas y minisculas
     // g cambia toda la linea
     // m cambia multiples lineas
-    var textoCifrado = texto.replace(/e/igm,"enter");
-    var textoCifrado = textoCifrado.replace(/o/igm,"ober");
+    var textoCifrado = texto.replace(/e/igm, "enter");
+    var textoCifrado = textoCifrado.replace(/o/igm, "ober");
     var textoCifrado = textoCifrado.replace(/i/igm, "imes");
-    var textoCifrado = textoCifrado.replace(/a/igm,"ai");
-    var textoCifrado = textoCifrado.replace(/u/igm,"ufat");
+    var textoCifrado = textoCifrado.replace(/a/igm, "ai");
+    var textoCifrado = textoCifrado.replace(/u/igm, "ufat");
 
     document.getElementById("imgDer").style.display = "none";
     document.getElementById("texto").style.display = "none";
@@ -18,3 +18,22 @@ function cifrar() {
     document.getElementById("inputtexto").innerHTML = " ";
     document.getElementById("copiar").style.display = "inherit";
 }
+
+// Agregamos la funcion descifrar
+function descifrar() {
+    // declaramos nuetsras variables
+    var texto = document.getElementById("inputtexto").value.toLocaleLowerCase();
+    var textoCifrado = texto.replace(/enter/igm,"e");
+    var textoCifrado = textoCifrado.replace(/ober/igm,"o");
+    var textoCifrado = textoCifrado.replace(/imes/igm,"i");
+    var textoCifrado = textoCifrado.replace(/ai/igm,"a");
+    var textoCifrado = textoCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("texto2").innerHTML = textoCifrado;
+    document.getElementById("inputtexto").innerHTML = " ";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
